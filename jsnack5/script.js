17a9a07067f269be75ff56ef91f89d252d4d1e84
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-28 16:33:28 +0200

    logical steps

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -1,3 +1,10 @@
 // Crea un array vuoto.
 // Chiedi per 6 volte all’utente di inserire un numero,
 // se è dispari inseriscilo nell’array.
+
+// 1.Creare un array vuoto
+
+// 2. chiedere all'utente di inserire per 6 volte un numero -> for
+
+// 3. Inserire i numeri nella lista
+//  3.1 Controllare che il numero sia dispari
