2d9f9b6b04d16dc1c61ce4ef790e6a34f8ca3d7f
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-28 14:32:35 +0200

    logical steps

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,2 +1,10 @@
 // L’utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore.
+
+// 1. Far inserire all'utente due numeri -> Prompt
+
+// 2. Confrontare i due numeri
+//  2.1 Confronto il primo numero con il secondo -> If
+
+// 3. Stampo il numero maggiore -> console.log
+
