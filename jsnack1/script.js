6311198d426dc92165c72bfa2c3050ef8b4f28a5
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-28 14:42:42 +0200

    finished jsnack1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -8,3 +8,21 @@
 
 // 3. Stampo il numero maggiore -> console.log
 
+// Inserimento dei numeri
+const num1 = parseInt(prompt("Inserisci un numero: "));
+const num2 = parseInt(prompt("Inserisci un altro numero: "));
+
+// Confronto dei due numeri
+if (num1 > num2) {
+// Se se num1 è maggiore di num2, stampa num1
+    console.log("Il numero più grande è ", num1);
+
+} else if (num1 < num2) {
+    // Se se num1 è minore di num2, stampa num2
+    console.log("Il numero più grande è ", num2);
+
+} else {
+    // Altrimenti sono uguali
+    console.log("I numeri sono uguali");
+
+}
