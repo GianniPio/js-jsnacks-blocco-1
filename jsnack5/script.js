009c0c084097ea65a386db431b3a6285c7eba66c
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-28 16:37:16 +0200

    finished jsnack6

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -8,3 +8,18 @@
 
 // 3. Inserire i numeri nella lista
 //  3.1 Controllare che il numero sia dispari
+
+const listNum = [];
+
+for (let i = 0; i < 6; i++) {
+
+    let num = parseInt(prompt("Inserisci un numero"));
+
+    if (num % 2 != 0) {
+        
+        listNum.push(num);
+
+    }
+}
+
+console.log(listNum);
