d381bf1526c401d826219adfd7311ca946273a3a
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-28 22:12:32 +0200

    logical steps

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,2 +1,11 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
+
+// 1. Chiedere all'utente 10 numeri
+    //  1.1 Creare un array vuoto
+    //  1.2 Far inserire 10 numeri -> for/prompt
+
+// 2. Stampare la somma dei numeri inseriti
+    // 2.1 Valora sommma iniziale = 0
+    // 2.2 Sommare nel valore somma i valori dell'array
+
