75fff59a4d17c6ae18edcf90e90a44a9229927dc
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-28 15:42:38 +0200

    logical steps

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,2 +1,15 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
 // chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+// 1. Creare una lista contente il nome degli invitati
+
+// 2. Chiedere all'utente il proprio nome
+
+// 3. Confrontare il nome dell'utente con quelli della lista
+//  3.1 Creo una variabile booleana
+//  3.2 Confronto il nome dell'utente con quelli della lista -> for
+//  3.3 Uso la variabile booleana per capire se il nome è presente -> if
+
+// 4. Comunico se può partecipare o no -> if
+//  4.1 Se la variabile bolleana è vera, allora può partecipare
+//  4.2 Altrimenti no
