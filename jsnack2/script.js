61b13c8a105d082fdfdeb5ad3d41a4e7dcb2b558
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-28 15:05:20 +0200

    logical steps

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -10,21 +10,24 @@
 //  3.1 Confronto la prima parola con la seconda, se è più corta stampo
 //  3.2 Altrimenti stampo prima la seconda e poi la prima
 
-
+// Inserimento parole
 const fistWord = prompt("Inserisci una parola");
-
 const secondWord = prompt("Inserisci un'altra parola");
 
+// confronto della lunghezza delle parole
 if (fistWord.length < secondWord.length) {
     
+    // se la prima è più piccola della seconda, stampa la prima e poi la seconda
     console.log(fistWord);
     console.log(secondWord);
 
 } else if (fistWord.length > secondWord.length) {
 
+    // se la prima è più grande della seconda, stampa la seconda e poi la prima
     console.log(secondWord);
     console.log(fistWord);
 } else {
 
+    // sono uguale
     console.log("Le parola hanno la stessa lunghezza");
 }
