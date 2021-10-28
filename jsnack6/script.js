8f6994806d2ed5660813304c640d42ea2efc187d
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-28 16:56:19 +0200

    logical steps

---
+++ jsnack6/script.js
@@ -0,0 +1,6 @@
+// Genera un array di 50 numeri random. Nell’array non devono esserci doppioni.
+
+//1. Generare un array di 50 numeri -> for
+
+//2. Controllare che nell'array non ci siamo numeri doppioni -> if
+
