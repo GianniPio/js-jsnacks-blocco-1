6b0faac4452ab7610c071e0e4a6fef34b7081df3
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-29 10:06:58 +0200

    correction

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -2,30 +2,19 @@
 // Il programma stampa la somma di tutti i numeri inseriti.
 
 // 1. Chiedere all'utente 10 numeri
-    //  1.1 Creare un array vuoto
-    //  1.2 Far inserire 10 numeri -> for/prompt
+    //  1.1 Far inserire 10 numeri -> for/prompt
 
 // 2. Stampare la somma dei numeri inseriti
     // 2.1 Valora sommma iniziale = 0
-    // 2.2 Sommare nel valore somma i valori dell'array
+    // 2.2 Sommare nel valore somma i valori inseriti
 
 
-    const listNum = [];
- 
+    let somma = 0;
 
     for (let i = 0; i < 10; i++) {
         
-        listNum[i] = parseInt(prompt("Inserisci un numero"));
-        
+        let number = parseInt(prompt("Inserisci un numero"));
+         somma += number;
     }
 
-   let somma = 0;
-
-   for (let i = 0; i < listNum.length; i++) {
-       
-        somma += listNum[i];
-       
-   }
-
-    console.log(listNum);
     console.log(somma);
