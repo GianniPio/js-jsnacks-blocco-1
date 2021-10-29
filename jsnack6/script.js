22fb9e023fe91dbf683f8b31fb5121fa18b9d689
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-29 10:34:41 +0200

    finished jsnack6

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -1,25 +1,36 @@
 // Genera un array di 50 numeri random. Nell’array non devono esserci doppioni.
 
-//1. Generare un array di 50 numeri -> for
+//1. Generare un array di 50 numeri -> while
+    //  1.1 Inserire numeri random
 
-//2. Controllare che nell'array non ci siamo numeri doppioni -> if
+
+//2. Controllare che nell'array non ci siamo numeri doppioni
+    // 2.1 Tramite variabile controllare che non si siano doppioni ->
+    // 2.2 Se ci sono doppioni la variabile diventa vera, se è falsa salva il numero nell'array -> for/if
 
 const arrayNum = [];
-const arrRandom = [];
-let i = 0;
+
 while (arrayNum.length < 50) {
 
     let number = Math.floor(Math.random() * 100) + 1;
 
-    arrRandom.push(number);
+    let duplicate = false;
 
-   for (let i = 0; i < arrRandom.length + 1; i++) {
+    for (let i = 0; i < arrayNum.length; i++) 
+        
+        if (arrayNum[i] === number) {
 
+            duplicate = true;
+        }
+        
+    }
+
+    if (duplicate == false) {
+        
+        arrayNum.push(number);
+
+    }
     
-       
-   }
-
-    console.log(arrayNum[number]);
-
 }
 
+console.log(arrayNum);
